cea1f8aa85331f968d55beacf8aca71486f71509
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-05-05 15:44:36 +0200

    deploy to master

--- setup.js
+++ setup.js
@@ -1,6 +1,7 @@
+const { client } = require('./src/utils/db.js');
+
 const { Room } = require('./src/models/room.model.js');
 const { User } = require('./src/models/user.model.js');
 const { Message } = require('./src/models/message.model.js');
-const { client } = require('./src/utils/db.js');
 
 client.sync({ force: true });
--- src/utils/db.js
+++ src/utils/db.js
@@ -3,12 +3,13 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const client = new Sequelize({
-  port: process.env.DB_PORT,
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT || '5432',
+  host: process.env.DB_HOST || 'localhost',
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || '123123',
+  database: process.env.DB_DATABASE || 'postgres',
   dialect: 'postgres',
+  logging: false,
 });
 
 module.exports = {
